1d8e5e6f2d6cd4fdcdab5c7e20ca8a2054a0e11b
Reece Breidenbach <reece.breidenbach@ndus.edu>
2024-11-20 13:44:20 -0600

    new Class Student

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Fall-2024.Models
+{
+    public class Student
+{
+    public int Id { get; set; }
+    public string? StudentFirstName { get; set; }
+    public string? StudentLastName { get; set; }
+
+}
+}
